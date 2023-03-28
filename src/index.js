cea004cde3518686b09e1badfc93affe36708ace
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-03-28 19:36:34 +0100

    project scafold && webpack, tailwind config

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { camelCase  } from "lodash";
+
+import './style.css';
+
+console.log("Hello World js!")
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ['./public/*.html'],
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
