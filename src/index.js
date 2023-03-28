20b0f4493efb314e2f0be342131ae55c05187f06
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-03-28 22:04:09 +0100

    todo list structure

--- src/index.js
+++ src/index.js
@@ -1,2 +1,59 @@
 import './style.css';
 import './input.css';
+
+const todoList = document.querySelector('#todo-list');
+
+class Todo {
+    constructor (decription, completed, index) {
+        this.decription = decription;
+        this.completed = completed;
+        this.index = index;
+    }
+}
+
+class Todos {
+  constructor() {
+    this.todos = [
+        new Todo('Learn JavaScript', false, 0),
+        new Todo('Learn Angular', false, 1),
+        new Todo('Learn React', false, 2),
+        new Todo('Learn Vue', false, 3),
+    ];
+  }
+
+  add(todo) {
+    this.todos.push(todo);
+  }
+
+  remove(todo) {
+    this.todos = this.todos.filter(t => t !== todo);
+  }
+
+    render() { 
+        todoList.innerHTML = '';
+        this.todos.forEach(todo => {
+        const div = document.createElement('div');
+        div.classList.add('flex', 'justify-between', 'items-center', 'p-4', 'border-b', 'border-gray-200');
+        const div2 = document.createElement('div');
+        div2.classList.add('flex', 'items-center');
+        const input = document.createElement('input');
+        input.type = 'checkbox';
+        input.classList.add('mr-2');
+        const p = document.createElement('p');
+        p.classList.add('text-gray-500');
+        p.textContent = todo.decription;
+        const i = document.createElement('i');
+        i.classList.add('fa', 'fa-up-down-left-right', 'text-sm', 'cursor-pointer');
+        i.setAttribute('aria-hidden', 'true');
+
+        div2.appendChild(input);
+        div2.appendChild(p);
+        div.appendChild(div2);
+        div.appendChild(i);
+        todoList.appendChild(div);
+        });
+    }
+}
+
+const todos = new Todos();
+todos.render();
