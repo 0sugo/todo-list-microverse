a3a1e8f3515bd905f771dcde7f0e6c97420ad151
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-03-29 08:51:05 +0100

    Add remove, save and markcomplete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import './style.css';
 import './input.css';
-import { addTodo, removeTodo } from './todoCrud';
+import { addTodo, removeTodo, markCompleted } from './todoCrud';
 
 console.log('Hello world');
 
 const todoList = document.querySelector('#todo-list');
 const todoForm = document.querySelector('#todo-form');
 const input = document.querySelector('#todo-input');
+
+const showTrash = (index) => {
+  const trash = document.querySelector('.fa-trash');
+  const ellipsis = document.querySelector('.fa-ellipsis-v');
+  const todo = document.getElementById(index);
+  todo.classList.add('bg-orange-100');
+  ellipsis.classList.add('hidden');
+  trash.classList.remove('hidden');
+};
 
 export class Todo {
   constructor(decription, completed, index) {
@@ -32,27 +41,59 @@ class Todos {
     });
   }
 
+  save() {
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+
+  retrieve() {
+    const todos = JSON.parse(localStorage.getItem('todos'));
+    if (todos) {
+      this.todos = [...this.todos, ...todos];
+    }
+  }
+
   render() {
     todoList.innerHTML = '';
     this.todos.forEach((todo) => {
       const div = document.createElement('div');
+      div.id = todo.index;
       div.classList.add('flex', 'justify-between', 'items-center', 'p-4', 'border-b', 'border-gray-200');
       const div2 = document.createElement('div');
       div2.classList.add('flex', 'items-center');
       const input = document.createElement('input');
       input.type = 'checkbox';
+      input.checked = todo.completed;
+      input.addEventListener('change', () => {
+        markCompleted(todo.index);
+        input.checked = todo.completed;
+      });
       input.classList.add('mr-2');
       const p = document.createElement('p');
-      p.classList.add('text-gray-500');
+      p.classList.add('text-gray-700');
+      if (todo.completed) {
+        p.classList.add('line-through');
+      }
       p.textContent = todo.decription;
+      const div3 = document.createElement('div');
       const i = document.createElement('i');
-      i.classList.add('fa', 'fa-ellipsis-v', 'text-sm', 'cursor-move', 'text-gray-500', 'hove');
+      i.addEventListener('click', () => {
+        showTrash(todo.index);
+      });
+      i.classList.add('fa', 'fa-ellipsis-v', 'text-sm', 'cursor-move', 'text-gray-500');
       i.setAttribute('aria-hidden', 'true');
+      const i2 = document.createElement('i');
+      i2.addEventListener('click', () => {
+        removeTodo(todo.index);
+      });
+      i2.classList.add('fa', 'fa-trash', 'text-sm', 'cursor-move', 'text-gray-500', 'hidden');
+      i2.setAttribute('aria-hidden', 'true');
 
       div2.appendChild(input);
       div2.appendChild(p);
       div.appendChild(div2);
-      div.appendChild(i);
+      div3.appendChild(i);
+      div3.appendChild(i2);
+      div.appendChild(div3);
       todoList.appendChild(div);
     });
   }
@@ -70,4 +111,5 @@ todoForm.addEventListener('submit', (e) => {
 });
 
 export const todos = new Todos();
+todos.retrieve();
 todos.render();
--- src/todoCrud.js
+++ src/todoCrud.js
@@ -4,10 +4,21 @@ import { todos } from './index';
 export const addTodo = (description) => {
     const todo = new Todo(description, false, todos.todos.length);
     todos.add(todo);
+    todos.save();
     todos.render();
 };
 
 export const removeTodo = (id) => {
     todos.remove(todos.todos[id]);
+    todos.todos.forEach((t, i) => {
+        t.index = i;
+    });
+    todos.save();
+    todos.render();
+}
+
+export const markCompleted = (id) => {
+    todos.todos[id].completed = !todos.todos[id].completed;
+    todos.save();
     todos.render();
 }
